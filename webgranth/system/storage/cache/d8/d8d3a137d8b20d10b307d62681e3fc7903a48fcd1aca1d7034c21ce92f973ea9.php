bd34e339a49999d34a89220e74b9bc4b1707df88
Nguyễn Sinh Cầu <sinhcautb.ictu@gmail.com>
2018-03-27 11:58:18 +0700

    First commit

---
+++ webgranth/system/storage/cache/d8/d8d3a137d8b20d10b307d62681e3fc7903a48fcd1aca1d7034c21ce92f973ea9.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_d72cc351468c84d69a8b5d5c032ef4e896c7da81deb4bb3ea73b6a92da07778d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
