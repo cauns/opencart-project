c955acf87bf83096dc5b4177589c8813ae2a7e84
Nguyễn Sinh Cầu <sinhcautb.ictu@gmail.com>
2018-03-27 15:19:22 +0700

    re first commit

---
+++ webgranth/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/GITHUB/webgranth/admin/');
+define('HTTP_CATALOG', 'http://localhost/GITHUB/webgranth/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/GITHUB/webgranth/admin/');
+define('HTTPS_CATALOG', 'http://localhost/GITHUB/webgranth/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Ampps/www/GITHUB/webgranth/admin/');
+define('DIR_SYSTEM', 'D:/Ampps/www/GITHUB/webgranth/system/');
+define('DIR_IMAGE', 'D:/Ampps/www/GITHUB/webgranth/image/');
+define('DIR_STORAGE', 'D:/Ampps/www/GITHUB/webgranth/storage/');
+define('DIR_CATALOG', 'D:/Ampps/www/GITHUB/webgranth/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mysql');
+define('DB_DATABASE', 'webgranth');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocwebgranth_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
